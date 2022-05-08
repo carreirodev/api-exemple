4d4d484a7f0bdfe5eecf1c58016d8194ef4970e5
Eduardo Carreiro <eduardo@wpcwebsolutions.com>
2022-05-08 09:59:44 -0300

    added pass validation

---
+++ src/controllers/authController.js
@@ -0,0 +1,32 @@
+const Usuarios = require("../models/Usuarios");
+const bcrypt = require("bcryptjs");
+
+const authController = {
+	async login(req, res) {
+		try {
+			const { email, senha } = req.body;
+			console.log(email);
+			const userLogado = await Usuarios.findOne({
+				where: {
+					email
+				}
+			});
+
+			if (!userLogado) {
+				return res.status(404).json("Usuario inexiste na base de dados");
+			} else {
+				if (!bcrypt.compareSync(senha, userLogado.senha)) {
+					return res.status(401).json("senha incorreta");
+				}
+			}
+
+			return res.json("logado");
+		} catch (error) {
+			console.log("Login Incorreto");
+			console.error(error);
+			return res.status(500).json("Erro ao executer login");
+		}
+	}
+};
+
+module.exports = authController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,9 @@ const filmesController = require("../controllers/filmesController");
 const usuariosController = require("../controllers/usuariosController");
 
 const cadastroUserValidator = require("../validators/cadastroUserValidator");
+const loginValidator = require("../validators/loginValidator");
+
+const authController = require("../controllers/authController");
 
 const routes = express.Router();
 
@@ -12,4 +15,12 @@ routes.get("/movies", filmesController.listar);
 
 routes.post("/usuarios", cadastroUserValidator, usuariosController.cadastrar);
 
+routes.post("/login", loginValidator, authController.login);
+
 module.exports = routes;
+
+// GET trazer (SELECT)
+// POST enviar (LOGIN)
+// PUT enviar (INSERIR/ALTERAR)
+// DELETE enviar (DELETE)
+// PATCH enviar (ALTERAR)
